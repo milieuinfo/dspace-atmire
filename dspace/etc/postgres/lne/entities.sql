804ad4d3e7125408ee378b4a24beca4c9aef7c2b
Antoine Snyers <antoine@atmire.com>
2014-11-21 16:26:32 +0100

    22031: "if not exists" not available on psql 8.3

--- dspace/etc/postgres/lne/entities.sql
+++ dspace/etc/postgres/lne/entities.sql
@@ -10,67 +10,25 @@ CREATE FUNCTION getnextid(t VARCHAR(40))
                       OR relname = $1 || '_' || $1 || '_id_seq'))
          AS INTEGER) AS RESULT; $$ LANGUAGE SQL;
 
+CREATE TABLE Type
+(
+  type_id               SERIAL PRIMARY KEY,
+  left_type             VARCHAR(256),
+  right_type            VARCHAR(256),
+  left_label            TEXT,
+  right_label           TEXT,
+  left_min_cardinality  INTEGER,
+  right_min_cardinality INTEGER,
+  left_max_cardinality  INTEGER,
+  right_max_cardinality INTEGER,
+  semantic_ruleset      VARCHAR(256)
+);
 
-CREATE OR REPLACE FUNCTION create_table_type()
-  RETURNS VOID AS
-  $$
-  BEGIN
-
-    IF EXISTS(
-        SELECT
-          *
-        FROM pg_catalog.pg_tables
-        WHERE tablename = 'Type'
-    )
-    THEN
-      RAISE NOTICE 'Table "type" already exists.';
-    ELSE
-      CREATE TABLE IF NOT EXISTS Type
-      (
-        type_id               SERIAL PRIMARY KEY,
-        left_type             VARCHAR(256),
-        right_type            VARCHAR(256),
-        left_label            TEXT,
-        right_label           TEXT,
-        left_min_cardinality  INTEGER,
-        right_min_cardinality INTEGER,
-        left_max_cardinality  INTEGER,
-        right_max_cardinality INTEGER,
-        semantic_ruleset      VARCHAR(256)
-      );
-    END IF;
-
-  END;
-  $$ LANGUAGE SQL;
-
-
-CREATE OR REPLACE FUNCTION create_table_relationship()
-  RETURNS VOID AS
-  $$
-  BEGIN
-
-    IF EXISTS(
-        SELECT
-          *
-        FROM pg_catalog.pg_tables
-        WHERE tablename = 'Relationship'
-    )
-    THEN
-      RAISE NOTICE 'Table "relationship" already exists.';
-    ELSE
-      CREATE TABLE IF NOT EXISTS Relationship
-      (
-        relationship_id SERIAL PRIMARY KEY,
-        left_id         INTEGER REFERENCES Item ON DELETE CASCADE,
-        type_id         INTEGER REFERENCES Type ON DELETE RESTRICT,
-        right_id        INTEGER REFERENCES Item ON DELETE CASCADE,
-        CONSTRAINT relationship_left_type_right_unique UNIQUE (left_id, type_id, right_id)
-      );
-    END IF;
-
-  END;
-  $$ LANGUAGE SQL;
-
-
-SELECT create_table_type();
-SELECT create_table_relationship();
+CREATE TABLE Relationship
+(
+  relationship_id SERIAL PRIMARY KEY,
+  left_id         INTEGER REFERENCES Item ON DELETE CASCADE,
+  type_id         INTEGER REFERENCES Type ON DELETE RESTRICT,
+  right_id        INTEGER REFERENCES Item ON DELETE CASCADE,
+  CONSTRAINT relationship_left_type_right_unique UNIQUE (left_id, type_id, right_id)
+);
--- dspace/modules/additions/src/main/java/com/atmire/dspace/database/AddLNEEntities.java
+++ dspace/modules/additions/src/main/java/com/atmire/dspace/database/AddLNEEntities.java
@@ -4,6 +4,7 @@ import org.apache.commons.io.FileUtils;
 import org.dspace.core.ConfigurationManager;
 import org.dspace.core.Context;
 import org.dspace.storage.rdbms.DatabaseManager;
+import org.dspace.storage.rdbms.TableRow;
 
 import java.io.File;
 
@@ -16,9 +17,14 @@ public class AddLNEEntities extends DatabaseLoader {
     @Override
     public void loadDatabaseChanges(Context context) throws Exception {
 
-        String sqlFile = ConfigurationManager.getProperty("dspace.dir") + File.separator + "etc" + File.separator + "postgres" + File.separator + "lne" + File.separator + "entities.sql";
-        String sql = FileUtils.readFileToString(new File(sqlFile), "UTF-8");
-        DatabaseManager.loadSql(sql);
+        TableRow type = DatabaseManager.row("Type");
+        if (type.toString().length() > 4) {
+            // if the table does not exist, type.toString() will be "type"
+            // otherwise it will have much more info
+            String sqlFile = ConfigurationManager.getProperty("dspace.dir") + File.separator + "etc" + File.separator + "postgres" + File.separator + "lne" + File.separator + "entities.sql";
+            String sql = FileUtils.readFileToString(new File(sqlFile), "UTF-8");
+            DatabaseManager.loadSql(sql);
+        }
 
     }
 }
