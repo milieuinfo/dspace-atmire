f4227056faed925a4f1d5694b93aca71a70eb07e
Antoine Snyers <antoine@atmire.com>
2014-12-08 11:00:30 +0100

    23427: Type lookup for a given item, null check

--- dspace/modules/additions/src/main/java/com/atmire/dspace/content/RelationshipObjectServiceFactory.java
+++ dspace/modules/additions/src/main/java/com/atmire/dspace/content/RelationshipObjectServiceFactory.java
@@ -44,38 +44,41 @@ public class RelationshipObjectServiceFactory {
     }
 
     /**
-     * @return A set of classes that can be used on getRelationshipObjectService(Class<T> type)
+     * Returns a set of classes that can be used on getRelationshipObjectService(Class<T> type)
      * where the resulting RelationshipObjectService's findByItem(context,item) will provide a non null RelationShipObject
      */
     public Set<Class<? extends RelationShipObject>> getRelationshipObjectClassesForItem(Context context, Item item) {
         Set<Class<? extends RelationShipObject>> classes = new HashSet<Class<? extends RelationShipObject>>();
 
+        if(item != null) {
 
-        // Get all loop types
-        RelationshipTypeService relationshipTypeService = new DSpace().getServiceManager().getServiceByName("relationshipTypeService", RelationshipTypeService.class);
-        Collection<RelationshipType> allRelationShipTypes = relationshipTypeService.findByExample(context, new RelationshipType());
-        Set<RelationshipType> loopTypes = new HashSet<RelationshipType>();
-        for (RelationshipType relationShipType : allRelationShipTypes) {
-            if (relationShipType.getLeftType().equals(relationShipType.getRightType())) {
-                loopTypes.add(relationShipType);
-            }
-        }
-
-        // See for which loop types a relationship exists with this item
-        RelationshipService relationshipService = new DSpace().getServiceManager().getServiceByName("relationshipService", RelationshipService.class);
-        for (RelationshipType loopType : loopTypes) {
-
-            Relationship example = new Relationship(null, item, item, loopType);
-            Relationship relationship = relationshipService.findByExampleUnique(context, example);
-            if (relationship != null) {
-                try {
-                    String leftType = loopType.getLeftType();
-                    Class<? extends RelationShipObject> theClass = (Class<? extends RelationShipObject>) Class.forName(leftType);
-                    classes.add(theClass);
-                } catch (ClassNotFoundException e) {
-                    log.error("Error", e);
+            // Get all loop types
+            RelationshipTypeService relationshipTypeService = new DSpace().getServiceManager().getServiceByName("relationshipTypeService", RelationshipTypeService.class);
+            Collection<RelationshipType> allRelationShipTypes = relationshipTypeService.findByExample(context, new RelationshipType());
+            Set<RelationshipType> loopTypes = new HashSet<RelationshipType>();
+            for (RelationshipType relationShipType : allRelationShipTypes) {
+                if (relationShipType.getLeftType().equals(relationShipType.getRightType())) {
+                    loopTypes.add(relationShipType);
                 }
             }
+
+            // See for which loop types a relationship exists with this item
+            RelationshipService relationshipService = new DSpace().getServiceManager().getServiceByName("relationshipService", RelationshipService.class);
+            for (RelationshipType loopType : loopTypes) {
+
+                Relationship example = new Relationship(null, item, item, loopType);
+                Relationship relationship = relationshipService.findByExampleUnique(context, example);
+                if (relationship != null) {
+                    try {
+                        String leftType = loopType.getLeftType();
+                        Class<? extends RelationShipObject> theClass = (Class<? extends RelationShipObject>) Class.forName(leftType);
+                        classes.add(theClass);
+                    } catch (ClassNotFoundException e) {
+                        log.error("Error", e);
+                    }
+                }
+            }
+
         }
 
         return classes;
