5fbee666e9b60c8f94c8c08e19d118a32db7c0c1
Jan Lievens <jan.lievens@gmail.com>
2015-03-05 08:01:22 +0100

    DSPACE-9 upload fix

--- dspace/bin/bulk/imjv-upload.sh
+++ dspace/bin/bulk/imjv-upload.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# USAGE: ./bulk-upload.sh <export_dir> <collection_id>
-# eg. ./bulk-upload.sh /path/to/dms-export acd/7
+# USAGE: ./bulk-upload.sh <export_dir> <collection_id> <year>
+# eg. ./bulk-upload.sh /path/to/dms-export acd/7 2004
 
-for i in 2004 2005 2006 2007 2008 2009 2010 2011 2012 2013
-do
-    %tomcat_data_dir%/dspace/bin/dspace-high-mem dsrun com.atmire.dspace.BulkUploadIMJV -d $1/$i -s milieuverslag.xsd -v -c $2 > ./import.$i.log
-done
+%tomcat_data_dir%/dspace/bin/dspace-high-mem dsrun com.atmire.dspace.BulkUploadIMJV -d $1/$3 -s milieuverslag.xsd -v -c $2 > ./import.$3.log
